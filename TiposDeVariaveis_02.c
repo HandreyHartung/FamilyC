67c7e517a224a2cff081c86248213da16746fe65
Handrey Hartung <handrey.hartungjob@gmail.com>
2024-01-07 12:34:12 -0300

    TiposDeVáriaveis
    Exemplo básico de " Tipos de variáveis ", Linguagem C

---
+++ TiposDeVariaveis_02.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+void main (){
+
+int a;
+int b;
+int c;
+
+printf("\n Insira o primeiro valor : \n "); // Exibindo na tela para inserir o primeiro valor //
+scanf("%d", &a); //  %d Declarando para acrescentar numeros INTEIROS // // &a Chamando a variavel a //
+printf(" \n O primeiro valor e de : %d \n ", a ); // Exibindo o primeiro valor acrescentado //
+
+printf("\n Insira o segundo valor : \n"); // Exibindo na tela para inserir o segundo valor //
+scanf("%d",&b); // %d Declarando para acrescentar numeros INTEIROS // // &b Chamando a variavel b //
+printf("\n O segundo valor e de : %d \n", b); // Exibindo o segundo valor acrescentado //
+
+printf("\n Insira o terceiro valor : \n "); // Exibindo na tela para inserir o terceiro valor //
+scanf("%d", &c); // %d Declarando para acrescentar numeros INTEIROS // // &c Chamando a variavel c //
+printf("\n O terceiro valor e de : %d \n", c); // Exibindo o terceiro valor acrescentado //
+
+printf("\n Agora efetue a soma dos números \n ", )
+
+return 0;
+
+}
