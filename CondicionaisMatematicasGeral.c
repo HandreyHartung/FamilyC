f81cd48b9bff08b8f88dc5390f1f8eedfe729557
Handrey Hartung <116693825+HandreyHartung@users.noreply.github.com>
2024-01-08 20:42:35 -0300

    CondicionaisMatematicasGeral.c
    Representação das Condicionais Matemáticas com a utilização do IF dentro do campo da codificação.

--- CondicionaisMatematicasGeral.c
+++ CondicionaisMatematicasGeral.c
@@ -1,16 +1,16 @@
-#include <stdio.h>
+#include <stdio.h>      // Acrescentando as bibliotecas //
 #include <stdlib.h>
  
-//Função principal do programa
-void main(){
+
+void main(){  //  Função principal //
  
     //Definindo Variáveis
-    int a = 5, b = 10, c = 15;
-    char d = 'x';
+    int a = 5, b = 10, c = 15; // Acrescentando variaveis do tipo inteiro //
+    char d = 'x'; // Acrescentando variavel char //
  
     //Maior
-    if(a > 2){
-        printf("\n %d eh maior que 2", a);
+    if(a > 2){   // Se a que resulta em 5 for maior que 2 o printf abaixo ira exibir a seguinte informação //
+        printf("\n %d eh maior que 2", a); // %d representamos que contactamos a variavel INT para ser exibida no printf, junto com a especificação da variavel a = 5 //
     }
  
     //Maior ou Igual
@@ -25,7 +25,7 @@ void main(){
  
     //Menor ou igual
     if(a <= 10){
-        printf("\n %d eh menor ou igual a 10", a);
+        printf("\n %d eh menor ou igual a 10", a);             // SE CASO VOCÊ ENTENDEU O EXEMPLO ACIMA EXPLICANDO PODEMOS REPRESENTAR OS DEMAIS DA MESMA PROPORÇÃO, POREM ALTERA A FORMA DE POSIÇÕES E AS VARIAVEIS //
     }
  
     //Diferente
@@ -36,7 +36,7 @@ void main(){
         printf("\n %c nao eh a", d);
     }
  
-    //Pausa o programa após executar
-    system("pause");
+                            
+    system("pause"); // Pausa para a execução do código //
  
 }
