484b1eb1d8d940c2a11ecbfe64913c811faa3121
Handrey Hartung <handrey.hartungjob@gmail.com>
2024-01-10 21:11:41 -0300

    Create CondicionaisBooleanas.c
    Breve explicação e bem simples de como  incrementar uma condicional booleana

---
+++ CondicionaisBooleanas.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+
+void main (){
+
+bool a = true; // Incluindo as condicionais Booleanas //
+bool b = false;
+
+if(a){ // Acrescentamos dessa maneira pois a variavel já reconhece o Bool mais o seu valor acrescentado  //
+    printf("\n a Letra A seria verdadeiro \n ");
+
+}
+
+system("pause");
+
+}
