29e19fbc6285caca76db02e2d22000d3c1ea225f
Handrey Hartung <handrey.hartungjob@gmail.com>
2024-01-25 22:54:09 -0300

    Create EstruturaDeRepetição_Exemplo.c
    Explicação breve da " Estrutura de repetição While "

---
+++ EstruturaDeRepetição_Exemplo.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h> // Incluindo bibliotecas
+
+void main (){ // Função principal //
+
+float a = 10;   // Declarando variaveis //
+
+while( a <= 15){ // Declarando estrutura de repetição WHILE //
+    printf("\n%.2f", a); // Exibindo na tela com \n = Nova linha / Proxima linha // %.2f = Contagem de casas dos números //
+
+a++; // Expressão de incremento //
+
+}
+
+return 0; //  é utilizada no final de uma função em C para indicar que a execução da função foi concluída com sucesso //
+
+}
