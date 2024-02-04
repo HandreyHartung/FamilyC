f3ff4efff47bdc805ec58ec687ab136eacbea855
Handrey Hartung <handrey.hartungjob@gmail.com>
2024-02-04 13:52:53 -0300

    Create EstruturaRepeticaoFORTabuada.c
    Estrutura de repetição FOR, script de uma tabuada

---
+++ EstruturaRepeticaoFORTabuada.c
@@ -0,0 +1,67 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+void main (){
+
+int a;
+int b;
+int c;
+int d;
+int e;
+int f;
+int g;
+int h;
+
+for (a = 1; a <= 10; a++){
+    printf("\n 2 X %d = %d ", a, 2 * a);
+}
+
+printf("\n");
+
+
+for (b = 1; b <= 10; b++){
+    printf("\n 3 X %d = %d ", b, 3 * b);
+}
+
+printf("\n");
+
+
+for(c = 1; c <= 10; c++){
+    printf("\n 4 X %d = %d ", c, 4 * c);
+}
+
+printf("\n");
+
+for(d = 1; d <= 10; d++){
+    printf("\n 5 X %d = %d ", d, 5 * d);
+}
+
+printf("\n");
+
+for(e = 1; e <= 10; e++){
+    printf("\n 6 X %d = %d ", e, 6 * e);
+}
+
+printf("\n");
+
+for(f = 1; f <= 10; f++){
+    printf("\n 7 X %d = %d ", f, 7 * f);
+}
+
+printf("\n");
+
+for(g = 1; g <= 10; g++){
+    printf("\n 8 X %d = %d ", g, 8 * g);
+
+}
+
+printf("\n");
+
+for(h = 1; h <= 10; h++){
+    printf("\n 9 X %d = %d ", h, 9 * h);
+}
+
+return 0;
+
+}
