32892d015d1d1d1b6ac03f6a4353257fefafd7eb
Handrey Hartung <handrey.hartungjob@gmail.com>
2024-02-12 22:03:30 -0300

    Create Constantes_Define.c
    Constante com DEFINE, utilizando a estrutura FOR. com a compilação da sequência de números, com a contagem dos numeros e a exibição única da variavel dentro do DEFINE.

---
+++ Constantes_Define.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+#define tam 10
+
+int main (){
+    printf("\n %d",tam); // Imprimindo na tela //
+
+    int i; //Definindo variavel //
+
+for(i=1; i<=tam; i++){ // Estrutura for // // Utilizando o seguinte exemplo, Se i e igual a 1 & i for menor ou igual a TAM, acrescentar no i ate chegar no limite de TAM //
+    printf("\n%d", i);
+
+}
+return 0;
+}
